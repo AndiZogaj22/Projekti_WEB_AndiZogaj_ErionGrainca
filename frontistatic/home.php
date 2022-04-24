6d3cb6950668cef1ee5f39871c3da5c01d0e8049
AndiZogaj22 <96266000+AndiZogaj22@users.noreply.github.com>
2022-04-24 22:11:30 +0200

    frontistatic (pjesa e pare e projektit)

---
+++ frontistatic/home.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website UPbeat</title>
+
+    <!-- google fonts cdn link  -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+<!-- heading section starts  -->
+<header>
+    <a href="#" class="logo"><i class="fas fa-headphones"></i>UpBeat</a>
+    <div id="menu" class="fas fa-bars"></div>
+    <nav class="navbar">
+        <ul>
+        <li><a class="active" href="home.php">home</a></li>
+            <li><a href="about.php">about</a></li>
+            <li><a href="features.php">features</a></li>
+            <li><a href="../login.php">product</a></li>
+            <li><a href="news.php">news</a></li>
+                     
+        </ul>
+    </nav>
+</header>
+<!-- heading section ends -->
+<!-- home section starts  -->
+<section class="home" id="home">
+    <div class="content">
+        <h1>Best quality products for you</h1>
+        <p>UPbeat është një dyqan online i paisjeve kompjuterike dhe telefonike me seli në Prishtinë. Produktet e saj përfshijnë kompjuterë desktop, laptopë, netbook, telefona celularë, pajisje rrjeti, monitorë, ruterë wi-fi, projektorë etj.</p>
+        <a href="#"><button class="btn">order now</button></a>
+    </div>
+    <div class="image">
+        <img src="homg-img.png" alt="">
+    </div>
+</section>
+<!-- home section ends -->
+
+<!-- footer section starts  -->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>why choose us?</h3>
+            <p>Nese jeni te interesuar qe te bleni paisje elektronike me qmimet me te volitshme ne treg jeni ne vendin e duhur.</p>
+        </div>
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#">home</a>
+            <a href="#">about</a>
+            <a href="#">features</a>
+            <a href="#">products</a>
+            <a href="#">contact</a>             
+        </div>
+        <div class="box">
+            <h3>Vendndodhja</h3>
+            <p> <i class="fas fa-map-marker-alt"></i> Prishtina, Kosovo 10000. </p>
+            <p> <i class="fas fa-globe"></i> www.UpBeat.com </p>
+            <p> <i class="fas fa-phone"></i> +123-456-7890. </p>
+        </div>
+    </div>
+    </section>
+    <!-- footer section ends -->
+    
+    
+    
+    
+    
+    
+    <!-- custom js file link  -->
+    <script src="script.js"></script>
+    
+    
+    </body>
+    </html>
